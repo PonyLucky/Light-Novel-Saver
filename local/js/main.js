83ec80ff241d703a21bf61d2701bf1031643fac6
Louis MARGOT <louis.margot@viacesi.fr>
2021-08-20 01:35:22 +0200

    Remove unuseful code

--- local/js/main.js
+++ local/js/main.js
@@ -16,7 +16,8 @@ var ln_lst = {
 	],
 	"www.novelpub.com": [
 		function() {
-			ln_type_www_novelpub_com();
+			// The same so I won't write a second time
+			ln_type_www_lightnovelworld_com();
 		},
 		"Insert the first chapter of the Light Novel"
 	],
--- local/js/www_novelpub_com.js
+++
@@ -1,4 +0,0 @@
-function ln_type_www_novelpub_com() {
-  // The same so I won't write a second time
-  ln_type_www_lightnovelworld_com();
-}
