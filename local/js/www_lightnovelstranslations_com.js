15de441f7324b64c2bfc5f98a9957692f7f42ba2
Louis MARGOT <louis.margot@viacesi.fr>
2021-08-19 18:48:09 +0200

    Use of 80 max char per line
    I use a screen vertically and it's not fun to scroll horizontally for every line

--- local/js/www_lightnovelstranslations_com.js
+++ local/js/www_lightnovelstranslations_com.js
@@ -13,10 +13,12 @@ function ln_type_www_lightnovelstranslations_com() {
 			ln_title = htmlDoc.querySelector("h1.entry-title").innerHTML;
 			
 			// Get the number of chapters of the Light Novel
-			ln_nb_chapters = htmlDoc.querySelector("div.entry-content").getElementsByTagName('a').length;
+			ln_nb_chapters = htmlDoc.querySelector("div.entry-content")
+				.getElementsByTagName('a').length;
 			
 			// Get the link of each chapter
-			let tmp = htmlDoc.querySelector("div.entry-content").getElementsByTagName('a');
+			let tmp = htmlDoc.querySelector("div.entry-content")
+				.getElementsByTagName('a');
 			for (let i = 0; i < tmp.length; i++) {
 				ln_link_chapters.push(tmp[i].getAttribute('href'));
 			}
@@ -40,10 +42,12 @@ function ln_type_www_lightnovelstranslations_com() {
 							let ln_chapter_title="", ln_chapter_content=new Array();
 
 							// Get the title chapter
-							ln_chapter_title = htmlDoc.querySelector("h1.entry-title").innerHTML;
+							ln_chapter_title = htmlDoc.querySelector("h1.entry-title")
+								.innerHTML;
 
 							// Get the title chapter
-							let tmp = htmlDoc.querySelector("div.entry-content").getElementsByTagName('p');
+							let tmp = htmlDoc.querySelector("div.entry-content")
+								.getElementsByTagName('p');
 							for (let i = 0; i < tmp.length; i++) {
 								if (tmp[i].className=="" && tmp[i].innerHTML!="&nbsp;") {
 									ln_chapter_content.push(tmp[i].outerHTML);
@@ -55,7 +59,8 @@ function ln_type_www_lightnovelstranslations_com() {
 							console.log((ln_nb_chapters_loaded++)+"/"+ln_nb_chapters);
 
 							// Then Put it in the content
-							ln_content += "<h1>"+ln_chapter_title+"</h1>\n"+ln_chapter_content;
+							ln_content += "<h1>"+ln_chapter_title+"</h1>\n"
+								+ln_chapter_content;
 						}
 						catch (error) {
 							console.info("This link isn't a chapter");
--- local/js/www_lightnovelworld_com.js
+++ local/js/www_lightnovelworld_com.js
@@ -10,19 +10,22 @@ function ln_type_www_lightnovelworld_com() {
 			let htmlDoc = html_parser(response);
 
 			// Get the title of the Light Novel
-			ln_title = htmlDoc.querySelector("div.titles").getElementsByTagName('a')[0].innerHTML;
+			ln_title = htmlDoc.querySelector("div.titles")
+				.getElementsByTagName('a')[0].innerHTML;
 
 			console.log("Title: "+ln_title);
 
-			//------------------------------------------------------------------------------
+			//------------------------------------------------------------------------
 			// Chapter variables
 			let ln_chapter_title="", ln_chapter_content=new Array();
 
 			// Get the title chapter
-			ln_chapter_title = htmlDoc.querySelector("div.titles").getElementsByTagName('H2')[0].innerHTML;
+			ln_chapter_title = htmlDoc.querySelector("div.titles")
+				.getElementsByTagName('H2')[0].innerHTML;
 
 			// Get the content of the chapter
-			let tmp = htmlDoc.getElementById('chapter-container').getElementsByTagName('p');
+			let tmp = htmlDoc.getElementById('chapter-container')
+				.getElementsByTagName('p');
 			for (let i = 0; i < tmp.length; i++) {
 				if (tmp[i].className=="") {
 					ln_chapter_content.push(tmp[i].outerHTML);
@@ -35,8 +38,9 @@ function ln_type_www_lightnovelworld_com() {
 			// Then Put it in the content
 			ln_content += "<h1>"+ln_chapter_title+"</h1>\n"+ln_chapter_content+"\n\n";
 
-			ln_link_next_chapter = htmlDoc.querySelector("a.chnav.next").getAttribute('href');
-			//------------------------------------------------------------------------------
+			ln_link_next_chapter = htmlDoc.querySelector("a.chnav.next")
+				.getAttribute('href');
+			//------------------------------------------------------------------------
 
 			// Then until there's a next chapter
 			while (isNextChapter) {
@@ -48,15 +52,17 @@ function ln_type_www_lightnovelworld_com() {
 						// Format the response
 						let htmlDoc = html_parser(response);
 
-						//------------------------------------------------------------------------------
+						//------------------------------------------------------------------
 						// Chapter variables
 						let ln_chapter_title="", ln_chapter_content=new Array();
 
 						// Get the title chapter
-						ln_chapter_title = htmlDoc.querySelector("div.titles").getElementsByTagName('H2')[0].innerHTML;
+						ln_chapter_title = htmlDoc.querySelector("div.titles")
+							.getElementsByTagName('H2')[0].innerHTML;
 
 						// Get the content of the chapter
-						let tmp = htmlDoc.getElementById('chapter-container').getElementsByTagName('p');
+						let tmp = htmlDoc.getElementById('chapter-container')
+							.getElementsByTagName('p');
 						for (let i = 0; i < tmp.length; i++) {
 							if (tmp[i].className=="") {
 								ln_chapter_content.push(tmp[i].outerHTML);
@@ -67,12 +73,17 @@ function ln_type_www_lightnovelworld_com() {
 						console.log(ln_chapter_title);
 
 						// Then Put it in the content
-						ln_content += "<h1>"+ln_chapter_title+"</h1>\n"+ln_chapter_content+"\n\n";
+						ln_content += "<h1>"+ln_chapter_title+"</h1>\n"
+							+ln_chapter_content+"\n\n";
 
-						ln_link_next_chapter = htmlDoc.querySelector("a.chnav.next").getAttribute('href');
+						ln_link_next_chapter = htmlDoc.querySelector("a.chnav.next")
+							.getAttribute('href');
 
-						if (htmlDoc.querySelector("a.chnav.next").classList.contains('isDisabled')) isNextChapter = false;
-						//------------------------------------------------------------------------------
+						if (
+							htmlDoc.querySelector("a.chnav.next").classList
+								.contains('isDisabled')
+						) isNextChapter = false;
+						//------------------------------------------------------------------
 					},
 					error: function () {
 						console.error("The chapter couldn't be loaded");
